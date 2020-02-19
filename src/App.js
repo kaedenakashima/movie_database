fe7696f4c1373b08016b4ab1bf518fea675d1936
Katie <Katie@Sandstone.local>
2020-02-19 20:05:22 +0900

    first commit ep8

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+class App extends Component {
+  state = {
+    input: 'Hello'
+  };
+
+  updateInput = event => {
+    this.setState({
+      input: event.target.value.trim()
+    });
+  };
+
+  submit = () => {
+    console.log(this.text.value);
+  };
+
+  render() {
+    return (
+      <div className='App'>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
+          <Welcome text='Welcome to Using Props' />
+        </header>
+        <p className='App-intro' ref={input => (this.text = input)}>
+          aloha
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        <h3>{this.state.input}</h3>
+        <input
+          type='text'
+          onChange={this.updateInput}
+          value={this.state.input}
+        />{' '}
+        <input type='text' ref={input => (this.text = input)} />
+        <button onClick={this.submit}>Show Value</button>
+      </div>
+    );
+  }
+}
+
+const name = function(input) {
+  this.input = input;
+};
+
+class Welcome extends Component {
+  render() {
+    const { text } = this.props;
+    return <h1 className='App-title'>{text}</h1>;
+  }
 }
 
 export default App;
